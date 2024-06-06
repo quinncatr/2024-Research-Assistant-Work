776745bd24bd7befdea26c414bcd34715b9e1d43
Andy Borch <aborch48@gmail.com>
2024-06-06 15:39:58 -0600

    Initial groundwork for implementing multi worker training from tutorial

--- andy/TFTutorials/MultiWorkerTraining/MultiWorkerTrainingTutorial.py
+++ andy/TFTutorials/MultiWorkerTraining/MultiWorkerTrainingTutorial.py
@@ -2,8 +2,8 @@ import json
 import os
 import sys
 
-os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
-os.environ.pop('TF_CONFIG', None)
+#os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+#os.environ.pop('TF_CONFIG', None)
 
 if '.' not in sys.path:
     sys.path.insert(0, '.')
@@ -18,3 +18,21 @@ batch_size = 64
 single_worker_dataset = mnist_setup.mnist_dataset(batch_size)
 single_worker_model = mnist_setup.build_and_compile_cnn_model()
 single_worker_model.fit(single_worker_dataset, epochs = 3, steps_per_epoch = 70)
+
+
+tf_config = {
+    'cluster': {
+        'worker':['localhost:1', 'localhost:2']
+    },
+    'task': {'type': 'worker', 'index': 0}
+}
+
+json.dumps(tf_config)
+
+tf_config['task']['index'] = 1
+
+
+strategy = tf.distribute.MultiWorkerMirrorStrategy()
+with strategy.scope():
+    multi_worker_model = mnist_setup.build_and_compile_cnn_model()
+
--- andy/TFTutorials/MultiWorkerTraining/mnist_setup.py
+++ andy/TFTutorials/MultiWorkerTraining/mnist_setup.py
@@ -3,6 +3,8 @@
 import os
 import tensorflow as tf
 import numpy as np
+import sys
+import json
 
 def mnist_dataset(batch_size):
     (x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
@@ -26,4 +28,38 @@ def build_and_compile_cnn_model():
         optimizer = tf.keras.optimizers.SGD(learning_rate = 0.001),
         metrics = ['accuracy'])
     return model
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '-1'
+os.environ.pop('TF_CONFIG', None)
+
+if '.' not in sys.path:
+    sys.path.insert(0, '.')
+
+import tensorflow as tf
+
+import mnist_setup
+
+#This single worker test makes sure that everything works before going onto multi worker
+#NOTE: this single worker makes it run on the CPU, not the GPU due to line 5 
+batch_size = 64
+single_worker_dataset = mnist_setup.mnist_dataset(batch_size)
+single_worker_model = mnist_setup.build_and_compile_cnn_model()
+single_worker_model.fit(single_worker_dataset, epochs = 3, steps_per_epoch = 70)
+
+
+tf_config = {
+    'cluster': {
+        'worker':['localhost:1', 'localhost:2']
+    },
+    'task': {'type': 'worker', 'index': 0}
+}
+
+json.dumps(tf_config)
+
+tf_config['task']['index'] = 1
+
+
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+with strategy.scope():
+    multi_worker_model = mnist_setup.build_and_compile_cnn_model()
 

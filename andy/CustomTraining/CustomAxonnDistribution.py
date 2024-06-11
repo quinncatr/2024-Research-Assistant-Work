df749cc6ebec4984ab0bff5457530b79b2da1cba
Andy Borch <aborch48@gmail.com>
2024-06-11 13:11:47 -0600

    Added tracker to record time to complete model fit

--- andy/CustomTraining/CustomAxonnDistribution.py
+++ andy/CustomTraining/CustomAxonnDistribution.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from timeit import default_timer as timer
 
 device = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(device[0], True)
@@ -8,7 +9,7 @@ def create_model():
     inputs = tf.keras.Input(shape = (28, 28, 1))
     conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu')(inputs)
 
-    with tf.device('/CPU:0'):
+    with tf.device('/GPU:0'):
         pool1 = tf.keras.layers.MaxPooling2D(2, 2)(conv1)
         conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu')(pool1)
         pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
@@ -28,4 +29,9 @@ model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metr
 x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 
+startFit = timer()
 model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
+endFit = timer()
+
+print("TIME to FIT: " + str(endFit - startFit))
+

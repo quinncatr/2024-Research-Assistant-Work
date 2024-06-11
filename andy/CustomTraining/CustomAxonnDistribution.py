b6ef4ce154dd5b4b52a18bbca6b7d452d8716688
Andy Borch <aborch48@gmail.com>
2024-06-11 15:01:00 -0600

    Ran and timed model exclusively on CPU and GPU. (No distribution or layer splitting)

--- andy/CustomTraining/CustomAxonnDistribution.py
+++ andy/CustomTraining/CustomAxonnDistribution.py
@@ -9,11 +9,11 @@ def create_model():
     inputs = tf.keras.Input(shape = (28, 28, 1))
     conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu')(inputs)
 
-    with tf.device('/GPU:0'):
-        pool1 = tf.keras.layers.MaxPooling2D(2, 2)(conv1)
-        conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu')(pool1)
-        pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
-        flatten = tf.keras.layers.Flatten()(pool2)
+    #with tf.device('/GPU:0'):
+    pool1 = tf.keras.layers.MaxPooling2D(2, 2)(conv1)
+    conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu')(pool1)
+    pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
+    flatten = tf.keras.layers.Flatten()(pool2)
     
     dense1 = tf.keras.layers.Dense((64), activation = 'relu')(flatten)
     output = tf.keras.layers.Dense(10, activation = 'softmax')(dense1)
@@ -29,9 +29,16 @@ model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metr
 x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 
-startFit = timer()
-model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
-endFit = timer()
+with tf.device('/GPU:0'):
+    startFitG = timer()
+    model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
+    endFitG = timer()
 
-print("TIME to FIT: " + str(endFit - startFit))
+with tf.device('/CPU:0'):
+    startFitC = timer()
+    model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
+    endFitC = timer()
+
+print("TIME to FIT GPU: " + str(endFitG - startFitG))
+print("TIME to FIT CPU : " + str(endFitC - startFitC))
 

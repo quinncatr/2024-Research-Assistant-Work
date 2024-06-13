9ec94c113eec807738422f04e1dcdf85d7f28e1b
Andy Borch <aborch48@gmail.com>
2024-06-13 18:36:24 +0000

    VScode decided it didn't want to do work anymore so pushing to back up most recent copy of code

--- andy/CustomTraining/CustomAxonnDistribution.py
+++ andy/CustomTraining/CustomAxonnDistribution.py
@@ -9,20 +9,24 @@ tf.config.experimental.set_memory_growth(device[0], True)
 tf.config.experimental.set_virtual_device_configuration(device[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
 
 def create_model():
+    global createTime
     inputs = tf.keras.Input(shape = (28, 28, 1))
     conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu')(inputs)
-
-    #with tf.device('/GPU:0'):
+    createstart = timer()
+   # with tf.device('/GPU:0'):
     pool1 = tf.keras.layers.MaxPooling2D(2, 2)(conv1)
     conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu')(pool1)
     pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
     flatten = tf.keras.layers.Flatten()(pool2)
-    
+   # with tf.device('/CPU:0'):
     dense1 = tf.keras.layers.Dense((64), activation = 'relu')(flatten)
     output = tf.keras.layers.Dense(10, activation = 'softmax')(dense1)
 
     model = tf.keras.Model(inputs = inputs, outputs = output)
+    createEnd = timer()
+    createTime = createEnd - createstart
     return model
+
 
 model = create_model()
 
@@ -41,7 +45,7 @@ def gpu_model_fit():
     with jtop() as jetson:
             gpuData = pd.DataFrame(jetson.stats, index = [0])
             power = pd.DataFrame(jetson.power)
-            gpuPower = power['tot'][7]
+            gpuPower = power['tot'][2]
     with tf.device('/GPU:0'):
         startGPU = timer()
         model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
@@ -57,7 +61,7 @@ def cpu_model_fit():
     with jtop() as jetson:
             cpuData = pd.DataFrame(jetson.stats, index = [0])
             power = pd.DataFrame(jetson.power)
-            cpuPower = power['tot'][7]
+            cpuPower = power['tot'][2]
     with tf.device('/CPU:0'):
         startCPU = timer()
         model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
@@ -65,33 +69,48 @@ def cpu_model_fit():
     cpuEnergy = cpuPower * (endCPU - startCPU)
 
 def distributed_model_fit():
-    return
+    global startDist
+    global endDist
+    global distData
+    global distPower
+    global distEnergy
+    with jtop() as jetson:
+            distData = pd.DataFrame(jetson.stats, index = [0])
+            power = pd.DataFrame(jetson.power)
+            distPower = power['tot'][2]
+   # with tf.device('/CPU:0') and tf.device('/GPU:0'):
+       # startDist = timer()
+    model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
+       # endDist = timer()
+    distEnergy = distPower * (endDist - startDist)
 
 gpu_model_fit()
-cpu_model_fit()
+#cpu_model_fit()
 #distributed_model_fit()
 
 print("\n-----Time to Completion-----\n")
 print("Time to complete using the GPU: " + str(endGPU - startGPU) + " seconds.")
-print("Time to complete using the CPU: " + str(endCPU - startCPU) + " seconds")
-print("Time to complete when distributed between the CPU and GPU: " + str(-1) + " seconds")
+#print("Time to complete using the CPU: " + str(endCPU - startCPU) + " seconds")
+#print("Time to complete when distributed between the CPU and GPU: " + str(endDist - startDist) + " seconds")
 
 print("\n-----Power Consumption-----\n")
 print("Power consumption using the GPU: " + str(gpuPower) + " milliwatts")
-print("Power consumption using the CPU: " + str(cpuPower) + " milliwatts")
-print("Power consumption distributed between the CPU and GPU: " + str(-1) + " milliwatts")
+#print("Power consumption using the CPU: " + str(cpuPower) + " milliwatts")
+#print("Power consumption distributed between the CPU and GPU: " + str(distPower) + " milliwatts")
 
 print("\n-----Energy Usage-----\n")
 print("Energy usage using the GPU: " + str(gpuEnergy) + " millijoules")
-print("Energy usage using the CPU: " + str(cpuEnergy) + " millijoules")
-print("Energy usage distributed between the CPU and GPU: " + str(-1) + " millijoules")
+#print("Energy usage using the CPU: " + str(cpuEnergy) + " millijoules")
+#print("Energy usage distributed between the CPU and GPU: " + str(distEnergy) + " millijoules")
 
 print("\n-----Comparisons-----\n")
 
-#TODO: Add Energy consumption in joules or millijoules (power*time)
+
+print("Time to create model: " + str(createTime) + " seconds.")
+
 #TODO: distribute model between cpu and gpu (Like line 32-40)
 #TODO: distribute layers between cpu and gpu (Like line 12)
     # do data vs model parallelization? I think previous two todos are both model
 #TODO: find out if vic could help with this stuff
-#TODO: Figour out how to run it on a other datasets
 #TODO: Distribute 1 layer between cpu and gpu
+#TODO: fix power printing not matching UI

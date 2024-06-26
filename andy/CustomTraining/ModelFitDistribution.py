9fccbf1b58ebde0559f7a1dfc2d9c1e2665ea7b7
Andy-Borch <aborch48@gmail.com>
2024-06-26 16:58:43 -0600

    Refactored code by having 1 fit method that can fit using various accelerators rather than 3 seperate methods that were basically duplicate methods

--- andy/CustomTraining/ModelFitDistribution.py
+++ andy/CustomTraining/ModelFitDistribution.py
@@ -26,115 +26,75 @@ def create_model():
 model = create_model()
 
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
-
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
 
-def gpu_model_fit():
-    global startGPU
-    global endGPU
-    global gpuData
-    global gpuPower
-    global gpuEnergy
+def generic_model_fit(accelerator):
+    global start
+    global end
+    global time
+    global data
+    global power
+    global energy
     with jtop() as jetson:
-            gpuData = pd.DataFrame(jetson.stats, index = [0])
-            power = pd.DataFrame(jetson.power)
-            gpuPower = power['tot'][2]
-    with tf.device('/GPU:0'):
-        startGPU = timer()
-        model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
-        endGPU = timer()
-    gpuEnergy = round(gpuPower * (endGPU - startGPU), 5)
+            data = pd.DataFrame(jetson.stats, index = [0])
+            powerInfo = pd.DataFrame(jetson.power)
+            power = powerInfo['tot'][2]
+    if accelerator == '/GPU:0' or accelerator == '/CPU:0':
+        with tf.device(accelerator):
+            start = timer()
+            model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
+            end = timer()
+    elif accelerator == vpi.Backend.VIC:
+        with vpi.Backend.VIC:
+            start = timer()
+            model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
+            end = timer()
+    else:
+        return
+    time = round(end - start, 5)
+    energy = round(power * (end - start), 5)
 
-def cpu_model_fit():
-    global startCPU
-    global endCPU
-    global cpuData
-    global cpuPower
-    global cpuEnergy
-    with jtop() as jetson:
-            cpuData = pd.DataFrame(jetson.stats, index = [0])
-            power = pd.DataFrame(jetson.power)
-            cpuPower = power['tot'][2]
-    with tf.device('/CPU:0'):
-        startCPU = timer()
-        model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
-        endCPU = timer()
-    cpuEnergy = round(cpuPower * (endCPU - startCPU), 5)
+gpu = '/GPU:0'
+cpu = '/CPU:0'
+vic = 'vpi.Backend.VIC'
 
-def distributed_model_fit():
-    global startDist
-    global endDist
-    global distData
-    global distPower
-    global distEnergy
-    with jtop() as jetson:
-            distData = pd.DataFrame(jetson.stats, index = [0])
-            power = pd.DataFrame(jetson.power)
-            distPower = power['tot'][2]
-    startDist = timer()
-    model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
-    endDist = timer()
-    distEnergy = distPower * (endDist - startDist)
+generic_model_fit(gpu)
+gpuTime = round(end - start, 5) 
+gpuEnergy = round(energy / 1000, 5)
+gpuPower = power
 
-def vic_model_fit():
-    global startVic
-    global endVic
-    global vicData
-    global vicPower
-    global vicEnergy
-    with jtop() as jetson:
-        vicData = pd.DataFrame(jetson.stats, index = [0])
-        power = pd.DataFrame(jetson.power)
-        vicPower = power['tot'][2]
-    with vpi.Backend.VIC:
-        startVic = timer()
-        model.fit(x_train, y_train, epochs = 5, batch_size = 64, validation_data = (x_test, y_test))
-        endVic = timer()
-    vicEnergy = vicPower * (endVic - startVic)
+generic_model_fit(cpu)
+cpuTime = round(end - start, 5)
+cpuEnergy = round(energy / 1000, 5)
+cpuPower = power
 
-#gpu_model_fit()
-#cpu_model_fit()
-#distributed_model_fit()
-vic_model_fit()
-
-#gpuTime = round(endGPU - startGPU, 5) 
-#gpuEnergy = round(gpuEnergy / 1000, 5)
-
-#cpuTime = round(endCPU - startCPU, 5)
-#cpuEnergy = round(cpuEnergy / 1000, 5)
-
-#distTime = round(endDist - startDist, 5)
-#distEnergy = round(distEnergy / 1000, 5)
-
-vicTime = round(endVic - startVic, 5)
-vicEnergy = round(vicEnergy / 1000, 5)
-
+generic_model_fit(vic)
+vicTime = round(end - start, 5)
+vicEnergy = round(energy / 1000, 5)
+vicPower = power
 
 print("\n-----Time to Completion-----\n")
-#print("Time to complete using the GPU: " + str(gpuTime) + " seconds.")
-#print("Time to complete using the CPU: " + str(cpuTime) + " seconds")
+print("Time to complete using the GPU: " + str(gpuTime) + " seconds.")
+print("Time to complete using the CPU: " + str(cpuTime) + " seconds")
 print("Time to complete using VIC: " + str(vicTime) + " seconds")
-#print("Time to complete when distributed between the CPU and GPU: " + str(distTime) + " seconds")
 
 print("\n-----Power Consumption-----\n")
-#print("Power consumption using the GPU: " + str(gpuPower / 1000) + " watts")
-#print("Power consumption using the CPU: " + str(cpuPower / 1000) + " watts")
+print("Power consumption using the GPU: " + str(gpuPower / 1000) + " watts")
+print("Power consumption using the CPU: " + str(cpuPower / 1000) + " watts")
 print("Power consumption using VIC: " + str(vicPower / 1000) + " watts")
-#print("Power consumption distributed between the CPU and GPU: " + str(distPower / 1000) + " watts")
 
 print("\n-----Energy Usage-----\n")
-#print("Energy usage using the GPU: " + str(gpuEnergy) + " joules")
-#print("Energy usage using the CPU: " + str(cpuEnergy) + " joules")
+print("Energy usage using the GPU: " + str(gpuEnergy) + " joules")
+print("Energy usage using the CPU: " + str(cpuEnergy) + " joules")
 print("Energy usage using VIC: " + str(vicEnergy) + " joules")
-#print("Energy usage distributed between the CPU and GPU: " + str(distEnergy) + " joules")
 
 print("\n-----Comparisons-----\n")
-
 
 #print("Time to create model on CPU: " + str(createTime) + " seconds.")
 #print("Time to create model on GPU: " + str(createTime) + " seconds.")
 
 #TODO: Distribute between vic cpu/gpu
 #TODO: Distribute 1 layer between cpu and gpu
+#TODO: Find out why VIC and CPU stats are basically identical

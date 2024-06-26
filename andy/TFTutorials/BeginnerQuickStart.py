8033e20c8aec2617bf91f6ff2921c53c2a445df5
Andy-Borch <aborch48@gmail.com>
2024-06-26 11:28:18 -0600

    Make compatible with tf 1.15

--- andy/TFTutorials/BeginnerQuickStart.py
+++ andy/TFTutorials/BeginnerQuickStart.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
+import numpy as numpy
 
-device = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(device[0], True)
-tf.config.experimental.set_virtual_device_configuration(device[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+tf.enable_eager_execution()
 
 print("TensorFlow version: ", tf.__version__)
 

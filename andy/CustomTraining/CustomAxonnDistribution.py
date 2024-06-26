08ee0d33992e42cd17fe58d2ddb072aab40f0a44
Andy-Borch <aborch48@gmail.com>
2024-06-26 14:00:49 -0600

    alterd model creation method

--- andy/CustomTraining/CustomAxonnDistribution.py
+++ andy/CustomTraining/CustomAxonnDistribution.py
@@ -4,12 +4,12 @@ from timeit import default_timer as timer
 from jtop import jtop
 import pandas as pd
 
-def create_model(accelerator):
+def create_model():
     global createTime
     inputs = tf.keras.Input(shape = (28, 28, 1))
     conv1 = tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu')(inputs)
     createStart = timer()
-    with tf.device(accelerator):
+    with tf.device('/GPU:0'):
         pool1 = tf.keras.layers.MaxPooling2D(2, 2)(conv1)
         conv2 = tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu')(pool1)
         pool2 = tf.keras.layers.MaxPooling2D((2, 2))(conv2)
@@ -22,10 +22,7 @@ def create_model(accelerator):
     createTime = round((createEnd - createStart), 5)
     return model
 
-
-model = create_model('/GPU:0')
-#model = create_model('/CPU:0')
-
+model = create_model()
 
 model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
@@ -115,7 +112,6 @@ print("\n-----Comparisons-----\n")
 #print("Time to create model on CPU: " + str(createTime) + " seconds.")
 print("Time to create model on GPU: " + str(createTime) + " seconds.")
 
-#TODO: distribute model.fit
 #TODO: distribute layers between cpu and gpu (Like model.create)
 #TODO: find out if vic could help with this stuff
 #TODO: Distribute 1 layer between cpu and gpu

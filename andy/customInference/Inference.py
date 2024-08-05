acc4110b880e3bb3c7fd1534dd79f5384a2e8287
Andy-Borch <aborch48@gmail.com>
2024-08-05 11:10:58 -0600

    added warmup loop and method to determine which processor runs inference

--- andy/customInference/Inference.py
+++ andy/customInference/Inference.py
@@ -5,6 +5,7 @@ from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_
 from timeit import default_timer as timer
 from jtop import jtop
 import pandas as pd
+import vpi
 
 tf.enable_eager_execution()
 
@@ -25,48 +26,34 @@ def predict_image(img_path, top_k = 5):
     for i, (imagenet_id, label, score) in enumerate(decoded_preds):
         print(f"{i + 1}: {label} ({score*100:.2f}%)")
 
-#Sequential inference(default processors)
-start = timer()
-with jtop() as jetson:
-    data = pd.DataFrame(jetson.stats, index = [0])
-    energy = pd.DataFrame(jetson.power)
-    print("Image 1: \n")
-    img1_path = 'Library.png'
-    predict_image(img1_path, top_k = 7)
-    print("\nImage 2: \n")
-    img2_path = 'cat.png'
-    predict_image(img2_path, top_k = 7)
-    end = timer()
+def inference(processor):
+    global elapsedTime
+    global Current
+    global Power
+    global Voltage
+    start = timer()
+    with processor:
+        with jtop() as jetson:
+            data = pd.DataFrame(jetson.stats, index = [0])
+            energy = pd.DataFrame(jetson.power)
+            img1_path = 'Library.png'
+            predict_image(img1_path, top_k = 7)
+            img2_path = 'cat.png'
+            predict_image(img2_path, top_k = 7)
+            end = timer()
 
-elapsedTime = end - start
-current = energy['tot'][4]
-power = energy['tot'][2]
-voltage = energy["tot"][8]
+    elapsedTime = end - start
+    Current = energy['tot'][4]
+    Power = energy['tot'][2]
+    Voltage = energy["tot"][8]
 
+    print("Time to get inference results using "+ str(processor) + ": " + str(elapsedTime) + " seconds.")
+    print("Power consumed during inference" + str(processor) + ": " + str(Power) + " milliwatts.")
+    print("Voltage drawn suring inference" + str(processor) + ": " + str(Voltage) + " millivolts.")
 
-print("Time to get inference results: " + str(elapsedTime) + " seconds.")
-print("Power consumed during inference: " + str(power) + " milliwatts.")
-print("Voltage drawn suring inference: " + str(voltage) + " millivolts.")
+#warmup loop
+print("Warmup Loop")
+inference(vpi.Backend.CUDA)
 
-
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
-import os
-
-# Load a pre-trained MobileNetV2 model
-model = tf.keras.applications.MobileNetV2(weights='imagenet')
-
-model_name = "model"
-baseModel = os.path.join(os.path.dirname(os.path.abspath(__file__)),(model_name))
-pb_model  = os.path.join(os.path.dirname(os.path.abspath(__file__)),(model_name+"_pb")) 
-trt_model = os.path.join(os.path.dirname(os.path.abspath(__file__)),(model_name+"_trt")) 
-
-if not os.path.exists(pb_model):
-    os.mkdir(pb_model)
-
-if not os.path.exists(trt_model):
-    os.mkdir(trt_model)
-
-if not os.path.exists(baseModel):
-    os.mkdir(baseModel)
-
-model.save("test_saved_model.keras")
+inference(vpi.Backend.CUDA)
+inference(vpi.Backend.CPU)
